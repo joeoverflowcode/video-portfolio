20c840e97446bd00afacceac0efe3b3213d35e15
Joe Aguado <aguado.joe@gmail.com>
2025-10-30 22:22:01 -0500

    feat: hero and video assets with looping logic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,50 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Hero = () => {
+  // const [currentIndex, setCurrentIndex] = useState(1);
+  // const [hasClicked, setHasClicked] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [loadedVideos, setLoadedVideos] = useState(0);
+
+  // const totalVideos = 5;
+  // const nextVideoRef = useRef(null);
+  // const handleVideoLoad = () => {
+  //   setLoadedVideos((prev) => prev + 1);
+  // };
+  // const upcomingVideoIndex = (currentIndex % totalVideos) + 1
+  // const handleMiniVdClick = () => {
+  //   setHasClicked(true);
+  //   setCurrentIndex(upcomingVideoIndex);
+  // };
+
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+
+  const [loading, setLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 4;
+  const totalVideos = 6;
   const nextVideoRef = useRef(null);
+
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);
   };
 
+  useEffect(() => {
+    if (loadedVideos === totalVideos) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
+  const upcomingVideoIndex =
+    currentIndex === totalVideos ? 1 : currentIndex + 1;
+
   const handleMiniVdClick = () => {
     setHasClicked(true);
-    setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
+    setCurrentIndex((prev) => (prev === totalVideos ? 1 : prev + 1));
   };
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       <div
@@ -30,14 +58,32 @@ const Hero = () => {
           >
             <video
               ref={nextVideoRef}
-              src={getVideoSrc(currentIndex + 1)}
+              src={getVideoSrc(upcomingVideoIndex)}
               loop
+              muted
               id="current-video"
               className="size-64 origin-center scale-150 object-cover object-center"
               onLoadedData={handleVideoLoad}
             />
           </div>
         </div>
+        <video
+          ref={nextVideoRef}
+          src={getVideoSrc(currentIndex)}
+          loop
+          muted
+          id="next-video"
+          className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
+        <video
+          src={getVideoSrc(currentIndex)}
+          autoPlay
+          loop
+          muted
+          className="absolute left-0 top-0 size-full object-cover object-center"
+          onLoadedData={handleVideoLoad}
+        />
       </div>
     </div>
   );
