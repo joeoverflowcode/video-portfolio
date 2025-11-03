8158b440c8fd2aad8d8dae5acecb17352dc676fb
Joe Aguado <aguado.joe@gmail.com>
2025-11-03 08:42:01 -0600

    feat: add about section and gsap animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
-  // const [currentIndex, setCurrentIndex] = useState(1);
-  // const [hasClicked, setHasClicked] = useState(false);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [loadedVideos, setLoadedVideos] = useState(0);
-
-  // const totalVideos = 5;
-  // const nextVideoRef = useRef(null);
-  // const handleVideoLoad = () => {
-  //   setLoadedVideos((prev) => prev + 1);
-  // };
-  // const upcomingVideoIndex = (currentIndex % totalVideos) + 1
-  // const handleMiniVdClick = () => {
-  //   setHasClicked(true);
-  //   setCurrentIndex(upcomingVideoIndex);
-  // };
-
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
 
@@ -64,10 +52,63 @@ const Hero = () => {
     setCurrentIndex((prev) => (prev === totalVideos ? 1 : prev + 1));
   };
 
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+  const handleMainVideoLoad = () => setLoading(false);
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -105,7 +146,7 @@ const Hero = () => {
             muted
             className="absolute left-0 top-0 size-full object-cover transition-[object-position] duration-500"
             style={{ objectPosition: currentFocus }}
-            onLoadedData={handleVideoLoad}
+            onLoadedData={handleMainVideoLoad}
           />
         </div>
 
@@ -118,9 +159,9 @@ const Hero = () => {
               joe <br />
               agua<b>d</b>o
             </h1>
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-              Bridging the gap between <br />
-              business and techonology
+            <p className="mb-5 max-w-100 font-robert-regular text-blue-75 text-[28px] ">
+              Hey I'm Joe, a web developer
+              <br /> based out of Dallas, Texas.
             </p>
             <Button
               id="watch-trailer"
@@ -134,6 +175,10 @@ const Hero = () => {
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
         web<b>d</b>ev
       </h1>
+      <h1 className="special-font hero-heading absolute left-10 top-24 text-black">
+  joe <br />
+  agua<b>d</b>o
+</h1>
     </div>
   );
 };
